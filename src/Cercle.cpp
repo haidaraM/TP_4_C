27ba449e5a2aaadf48e8411500c45cfeff7a9b86
emilienbai <emilien.bai@insa-lyon.fr>
2015-01-23 15:11:29 +0100

    Classe Cercle

--- src/Cercle.cpp
+++ src/Cercle.cpp
@@ -38,6 +38,26 @@ void Cercle::Afficher(ostream & flux)const
     flux<<"C "<<nom<<" "<<centre.GetAbscisse()<<" "<<centre.GetOrdonnee()<<" "<<rayon<<"\r\n";
 }
 
+bool Cercle::InclusDans(Point p1, Point p2)const
+{
+	int absmin = centre.GetAbscisse() - rayon;
+	int absmax = centre.GetAbscisse() + rayon;
+	int ordmin = centre.GetOrdonnee() - rayon;
+	int ordmax = centre.GetOrdonnee() + rayon;
+	
+	if((absmin >= p1.GetAbscisse() && absmax <= p2.GetAbscisse()) || 
+		(absmin >= p2.GetAbscisse() && absmax <= p1.GetAbscisse()))
+	{
+		if ((ordmin >= p1.GetOrdonnee() && ordmax <= p2.GetOrdonnee()) ||
+			(ordmin >= p2.GetOrdonnee() && ordmax <= p1.GetOrdonnee()))
+			{
+				return true;
+			}
+	}
+	
+	return false;
+}
+
 
 //-------------------------------------------- Constructeurs - destructeur
 Cercle::Cercle ( const Cercle & unCercle )
