4dd6d9c49c45c13033e8b20ea67a3ceb1e131efe
Mohamed Haidara <mohamed.haidara@insa-lyon.fr>
2015-08-12 16:23:18 +0200

    Suppression "< cmd" des fichiers run et correction delimiteur csv

--- tests/printCSV.py
+++ tests/printCSV.py
@@ -1,5 +1,5 @@
 import csv
-resultReader = csv.reader(open('results.csv',"rb"),delimiter=';')
+resultReader = csv.reader(open('results.csv',"rb"),delimiter=',')
 for row in resultReader:
 	print row[0],'=>', row[5]
 
